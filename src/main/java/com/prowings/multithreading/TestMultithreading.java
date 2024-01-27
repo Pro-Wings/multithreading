010f3560efbe17f1d6f5ba920e3c84af18a37696
JD <jayprowings@gmail.com>
2024-01-27 09:22:32 +0530

    multithreading demo

---
+++ src/main/java/com/prowings/multithreading/TestMultithreading.java
@@ -0,0 +1,34 @@
+package com.prowings.multithreading;
+
+public class TestMultithreading {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		MyThread t1 = new MyThread();
+	
+		System.out.println("T1 state : "+t1.getState());
+		System.out.println("T1 isAlive : "+t1.isAlive());
+		System.out.println("T1 isDemon : "+t1.isDaemon());
+//		t1.setName("mythread1");
+		System.out.println(t1.getName());
+//		t1.setPriority(9);
+		System.out.println("Priority : "+t1.getPriority());
+		System.out.println(">>>>>>>>>>>");
+//		t1.start();
+
+		MyThread2 m2 = new MyThread2();
+
+		Thread t2 = new Thread(m2, "MyThread2");
+		
+//		t2.setPriority(1);
+		System.out.println(t2.getName());
+
+		t1.start();
+		t2.start();
+		System.out.println("T1 state : "+t1.getState());
+		System.out.println("T1 isAlive : "+t1.isAlive());
+		Thread.sleep(2000);
+		System.out.println("T2 state : "+t2.getState());
+	}
+
+}
