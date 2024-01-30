14f10605f39cde0f4c2786f9470c5ee87a4cc6f3
JD <jayprowings@gmail.com>
2024-01-30 08:58:54 +0530

    thread synchronization

---
+++ src/main/java/com/prowings/bookmyseatapp/MyThreadOne.java
@@ -0,0 +1,22 @@
+package com.prowings.bookmyseatapp;
+
+public class MyThreadOne extends Thread{
+	
+	Theater theater;
+	int seat;
+
+	public MyThreadOne(Theater theater, int seat) {
+		super();
+		this.theater = theater;
+		this.seat = seat;
+	}
+
+	public void run() {
+		
+		theater.bookSeats(seat);
+		
+	}
+	
+	
+
+}
